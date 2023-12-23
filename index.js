ec0ce1f1e3a555d8bc8139a49fa0e40012eefd2f
zainhanif3 <juttgee4011@gmail.com>
2023-12-23 16:18:10 +0500

    commit

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const path = require("path");
 const pug = require("pug");
-const port = 80;
-const mysql = require("mysql");
+const port = 5000;
+const mongoose = require("mongoose");
 const app = express();
+
 
 const staticPath = path.join(__dirname, "views");
 console.log(path.join(__dirname, "views"));
 app.use(express.static(staticPath));
-app.set("view engine","html");
-app.get("/",(req, res) => {
-    res.render("index");
-  });
-app.get("/about", (req, res)=> {
-    res.render("about")
+app.set("view engine", "html");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get("/sign-in", (req, res)=> {
-    res.render("sign-in");
+app.get("/sign-in", (req, res) => {
+  res.render("sign-in");
 });
-app.get("/sign-up", (req, res)=> {
-    res.render("sign-up");
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up");
 });
-app.get("/portal", (req, res)=> {
-    res.render("portal");
+
+app.get("/portal", (req, res) => {
+  res.render("portal");
 });
-app.get("/password", (req, res)=> {
-    res.render("password");
+app.get("/password", (req, res) => {
+  res.render("password");
 });
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
